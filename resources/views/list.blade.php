6d7a0398a9efe1a9b6231efeff5ddb062b887baa
NitayD <nitayd2001@gmail.com>
2022-12-18 15:11:26 +0600

    fix

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -14,7 +14,7 @@
     <article class="container-fluid mt-3">
         <h2 class="mb-4">{{ $title }}</h2>
         <div class="container-xxl">
-            <div class="row">
+            <div class="row justify-content-center">
                 @foreach ($list as $item)
                     <div class="col-12 col-md-4 my-3">
                         @include('partials.list_elements.'.$itemType, ['item' => $item])
