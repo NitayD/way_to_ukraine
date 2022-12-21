f01953d8049dd8a5df5c181cd478f3478aca962c
NitayD <nitayd2001@gmail.com>
2022-12-21 15:19:31 +0600

    add video

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -29,6 +29,18 @@
     </p>
 </article>
 
+<article class="container-xxl mt-3">
+    <h2>Інтерв'ю UA:Перший у програмі телемарафону</h2>
+    <iframe
+        style="width: 100%; height: 700px;"
+        src="https://www.youtube.com/embed/hmbRxrP-dPQ?start=15852"
+        title="Ракетні удари по залізничній інфраструктурі та візит держсекретаря США Блінкена | 25 квітня"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen>
+    </iframe>
+</article>
+
 @include('partials.how_we_work')
 
 @endsection
