469114a06962e46db169be69038638cc0f4b7ee6
NitayD <nitayd2001@gmail.com>
2022-12-23 01:35:06 +0600

    fix reqs table mobile

--- resources/views/partials/requisites.blade.php
+++ resources/views/partials/requisites.blade.php
@@ -2,7 +2,7 @@
     <h2 class="text-center mb-4">@lang('welcome.reqs')</h2>
     @isset($founders)
         <div class="container-xxl d-flex justify-content-center">
-            <div class="block block-secondary d-inline-flex my-3">
+            <div class="block block-secondary d-inline-flex my-3 block-table">
                 <h4><b>@lang('welcome.donation.title')</b></h4>
                 <table class="mx-auto">
                     <tbody>
@@ -27,7 +27,7 @@
             @isset($reqs)
                 @foreach ($reqs as $item)
                     <div class="col-12 col-md-6">
-                        <div class="block block-secondary my-3">
+                        <div class="block block-secondary my-3 block-table">
                             <h4><b>{{ $item->name }}</b></h4>
                             <table>
                                 <tbody>
@@ -46,7 +46,7 @@
             @isset($nogroup)
                 @if ($nogroup->count() > 0)
                     <div class="col-12 col-md-6">
-                        <div class="block block-secondary my-3">
+                        <div class="block block-secondary my-3 block-table">
                             <h4><b>-</b></h4>
                             <table>
                                 <tbody>
